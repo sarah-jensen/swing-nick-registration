be4d4735b79b5b8d7baf4649f297a921078e00ef
Siofra <122956133+sarah-jensen@users.noreply.github.com>
2023-10-08 20:28:48 -0700

    removing node_modules

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,33 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from '@apollo/client';
+
+import {Navbar} from './components/Navbar.js';
+
+import './style/App.css';
+
 import { UserInfo } from './components'
 
 import { Container } from 'react-bootstrap';
 
  function App() {
-  return 
-  <Container fluid>
-    <UserInfo/>
-  </Container>;
+  return (
+    <div>
+    <ApolloProvider client={client}>
+          <Router>
+            <Navbar />
+            <Routes></Routes>
+          </Router>
+    </ApolloProvider>
+  </div>
+  );
 }
 
 export default App;
